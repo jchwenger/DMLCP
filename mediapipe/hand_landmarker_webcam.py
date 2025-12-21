9772c4f842d19cc79b2a1dc76a115384496bc1e1
jchwenger <34098722+jchwenger@users.noreply.github.com>
2025-12-21 23:42:09 +0100

    mediapipe | hand_landmarker_webcam

--- mediapipe/hand_landmarker_webcam.py
+++ mediapipe/hand_landmarker_webcam.py
@@ -1,3 +1,9 @@
+# --------------------------------------------------------------------------------
+# Commands:
+#   - 'q' to quit
+#   - '1' to toggle hand colours
+# --------------------------------------------------------------------------------
+
 import urllib
 import pathlib
 
@@ -12,24 +18,23 @@ import mediapipe as mp
 from mediapipe.tasks.python import vision
 from mediapipe.tasks.python.core import base_options as base_options_module
 
-# Drawing constants
+# Text overlay constants
+MARGIN = 10  # pixels
+FONT_SIZE = 2
+FONT_THICKNESS = 2
+
+# https://github.com/google-ai-edge/mediapipe/blob/9e4f898b22cf445c0ba7edc81ab4eb669fd71e89/mediapipe/python/solutions/drawing_utils.py#L32
+_BGR_CHANNELS = 3
+_VISIBILITY_THRESHOLD = 0.5
+_PRESENCE_THRESHOLD = 0.5
+
 WHITE_COLOR = (224, 224, 224)
-BLACK_COLOR = (0, 0, 0)
 RED_COLOR = (0, 0, 255)
 GREEN_COLOR = (0, 128, 0)
 BLUE_COLOR = (255, 0, 0)
 MAGENTA_COLOR = (255, 0, 255)
 CYAN_COLOR = (255, 255, 0)
 YELLOW_COLOR = (0, 255, 255)
-_BGR_CHANNELS = 3
-_VISIBILITY_THRESHOLD = 0.5
-_PRESENCE_THRESHOLD = 0.5
-
-# Text overlay constants
-MARGIN = 10  # pixels
-FONT_SIZE = 2
-FONT_THICKNESS = 2
-HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
 
 # https://github.com/google-ai-edge/mediapipe/blob/9e4f898b22cf445c0ba7edc81ab4eb669fd71e89/mediapipe/python/solutions/hands_connections.py#L16
 HAND_PALM_CONNECTIONS = ((0, 1), (0, 5), (9, 13), (13, 17), (5, 9), (0, 17))
@@ -44,25 +49,33 @@ HAND_RING_FINGER_CONNECTIONS = ((13, 14), (14, 15), (15, 16))
 
 HAND_PINKY_FINGER_CONNECTIONS = ((17, 18), (18, 19), (19, 20))
 
-HAND_CONNECTIONS = frozenset().union(*[
-    HAND_PALM_CONNECTIONS, HAND_THUMB_CONNECTIONS,
-    HAND_INDEX_FINGER_CONNECTIONS, HAND_MIDDLE_FINGER_CONNECTIONS,
-    HAND_RING_FINGER_CONNECTIONS, HAND_PINKY_FINGER_CONNECTIONS
-])
+HAND_CONNECTIONS = frozenset().union(
+    *[
+        HAND_PALM_CONNECTIONS,
+        HAND_THUMB_CONNECTIONS,
+        HAND_INDEX_FINGER_CONNECTIONS,
+        HAND_MIDDLE_FINGER_CONNECTIONS,
+        HAND_RING_FINGER_CONNECTIONS,
+        HAND_PINKY_FINGER_CONNECTIONS,
+    ]
+)
 
 
+# https://github.com/google-ai-edge/mediapipe/blob/9e4f898b22cf445c0ba7edc81ab4eb669fd71e89/mediapipe/python/solutions/drawing_utils.py#L39
 @dataclasses.dataclass
 class DrawingSpec:
-    """Drawing style spec."""
-
+    # Color for drawing the annotation. Default to the white color.
     color: Tuple[int, int, int] = WHITE_COLOR
+    # Thickness for drawing the annotation. Default to 2 pixels.
     thickness: int = 2
+    # Circle radius. Default to 2 pixels.
     circle_radius: int = 2
 
 
+# https://github.com/google-ai-edge/mediapipe/blob/9e4f898b22cf445c0ba7edc81ab4eb669fd71e89/mediapipe/python/solutions/drawing_utils.py#L49
 def _normalized_to_pixel_coordinates(
-    normalized_x: float, normalized_y: float, image_width: int, image_height: int
-) -> Optional[Tuple[int, int]]:
+    normalized_x, normalized_y, image_width, image_height
+):
     """Converts normalized value pair to pixel coordinates."""
     clamped_x = min(max(normalized_x, 0.0), 1.0)
     clamped_y = min(max(normalized_y, 0.0), 1.0)
@@ -71,19 +84,39 @@ def _normalized_to_pixel_coordinates(
     return (x_px, y_px)
 
 
+# https://github.com/google-ai-edge/mediapipe/blob/9e4f898b22cf445c0ba7edc81ab4eb669fd71e89/mediapipe/python/solutions/drawing_utils.py#L119
 def draw_landmarks(
-    image: np.ndarray,
-    landmark_list: Sequence,
-    connections: Optional[Iterable[Tuple[int, int]]] = None,
-    landmark_drawing_spec: Optional[
-        Union[DrawingSpec, Mapping[int, DrawingSpec]]
-    ] = DrawingSpec(color=RED_COLOR),
-    connection_drawing_spec: Union[
-        DrawingSpec, Mapping[Tuple[int, int], DrawingSpec]
-    ] = DrawingSpec(),
-    is_drawing_landmarks: bool = True,
+    image,
+    landmark_list,
+    connections=None,
+    landmark_drawing_spec=DrawingSpec(color=GREEN_COLOR),
+    connection_drawing_spec=DrawingSpec(color=BLUE_COLOR, thickness=2, circle_radius=0),
+    is_drawing_landmarks=True,
 ):
-    """Draws landmarks and connections (adapted from mediapipe drawing_utils)."""
+    """Draws the landmarks and the connections on the image.
+
+    Args:
+      image: A three channel BGR image represented as numpy ndarray.
+      landmark_list: A normalized landmark list proto message to be annotated on
+        the image.
+      connections: A list of landmark index tuples that specifies how landmarks to
+        be connected in the drawing.
+      landmark_drawing_spec: Either a DrawingSpec object or a mapping from hand
+        landmarks to the DrawingSpecs that specifies the landmarks' drawing
+        settings such as color, line thickness, and circle radius. If this
+        argument is explicitly set to None, no landmarks will be drawn.
+      connection_drawing_spec: Either a DrawingSpec object or a mapping from hand
+        connections to the DrawingSpecs that specifies the connections' drawing
+        settings such as color and line thickness. If this argument is explicitly
+        set to None, no landmark connections will be drawn.
+      is_drawing_landmarks: Whether to draw landmarks. If set false, skip drawing
+        landmarks, only contours will be drawed.
+
+    Raises:
+      ValueError: If one of the followings:
+        a) If the input image is not three channel BGR.
+        b) If any connetions contain invalid landmark index.
+    """
     if not landmark_list:
         return
     if image.shape[2] != _BGR_CHANNELS:
@@ -103,6 +136,7 @@ def draw_landmarks(
 
     if connections:
         num_landmarks = len(landmark_list)
+        # Draws the connections if the start and end landmarks are both visible.
         for connection in connections:
             start_idx, end_idx = connection
             if not (0 <= start_idx < num_landmarks and 0 <= end_idx < num_landmarks):
@@ -124,6 +158,8 @@ def draw_landmarks(
                     drawing_spec.thickness,
                 )
 
+    # Draws landmark points after finishing the connection lines, which is
+    # aesthetically better.
     if is_drawing_landmarks and landmark_drawing_spec:
         for idx, landmark_px in idx_to_coordinates.items():
             drawing_spec = (
@@ -131,6 +167,7 @@ def draw_landmarks(
                 if isinstance(landmark_drawing_spec, Mapping)
                 else landmark_drawing_spec
             )
+            # White circle border
             circle_border_radius = max(
                 drawing_spec.circle_radius + 1,
                 int(drawing_spec.circle_radius * 1.2),
@@ -142,6 +179,7 @@ def draw_landmarks(
                 WHITE_COLOR,
                 drawing_spec.thickness,
             )
+            # Fill color into the circle
             cv2.circle(
                 image,
                 landmark_px,
@@ -149,22 +187,6 @@ def draw_landmarks(
                 drawing_spec.color,
                 drawing_spec.thickness,
             )
-
-# Path to the model file
-model_path = pathlib.Path("models/hand_landmarker.task")
-
-# Check if the model file exists, if not, download it
-if not model_path.exists():
-    url = "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task"
-    print()
-    print(f"Downloading model from {url}...")
-    urllib.request.urlretrieve(url, model_path)
-    print(f"Model downloaded and saved as {model_path}")
-
-# Initialize MediaPipe HandLandmarker
-base_options = base_options_module.BaseOptions(model_asset_path=str(model_path))
-options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
-detector = vision.HandLandmarker.create_from_options(options)
 
 
 # Function to draw landmarks on the image
@@ -187,7 +209,9 @@ def draw_landmarks_on_image(rgb_image, detection_result, draw_subsets=False):
                 landmark_list=hand_landmarks,
                 connections=HAND_PALM_CONNECTIONS,
                 landmark_drawing_spec=None,
-                connection_drawing_spec=DrawingSpec(color=MAGENTA_COLOR, thickness=2, circle_radius=0),
+                connection_drawing_spec=DrawingSpec(
+                    color=MAGENTA_COLOR, thickness=2, circle_radius=0
+                ),
                 is_drawing_landmarks=False,
             )
             draw_landmarks(
@@ -195,7 +219,9 @@ def draw_landmarks_on_image(rgb_image, detection_result, draw_subsets=False):
                 landmark_list=hand_landmarks,
                 connections=HAND_THUMB_CONNECTIONS,
                 landmark_drawing_spec=None,
-                connection_drawing_spec=DrawingSpec(color=CYAN_COLOR, thickness=2, circle_radius=0),
+                connection_drawing_spec=DrawingSpec(
+                    color=CYAN_COLOR, thickness=2, circle_radius=0
+                ),
                 is_drawing_landmarks=False,
             )
             draw_landmarks(
@@ -203,7 +229,9 @@ def draw_landmarks_on_image(rgb_image, detection_result, draw_subsets=False):
                 landmark_list=hand_landmarks,
                 connections=HAND_INDEX_FINGER_CONNECTIONS,
                 landmark_drawing_spec=None,
-                connection_drawing_spec=DrawingSpec(color=YELLOW_COLOR, thickness=2, circle_radius=0),
+                connection_drawing_spec=DrawingSpec(
+                    color=YELLOW_COLOR, thickness=2, circle_radius=0
+                ),
                 is_drawing_landmarks=False,
             )
             draw_landmarks(
@@ -211,7 +239,9 @@ def draw_landmarks_on_image(rgb_image, detection_result, draw_subsets=False):
                 landmark_list=hand_landmarks,
                 connections=HAND_MIDDLE_FINGER_CONNECTIONS,
                 landmark_drawing_spec=None,
-                connection_drawing_spec=DrawingSpec(color=BLUE_COLOR, thickness=2, circle_radius=0),
+                connection_drawing_spec=DrawingSpec(
+                    color=BLUE_COLOR, thickness=2, circle_radius=0
+                ),
                 is_drawing_landmarks=False,
             )
             draw_landmarks(
@@ -219,7 +249,9 @@ def draw_landmarks_on_image(rgb_image, detection_result, draw_subsets=False):
                 landmark_list=hand_landmarks,
                 connections=HAND_RING_FINGER_CONNECTIONS,
                 landmark_drawing_spec=None,
-                connection_drawing_spec=DrawingSpec(color=GREEN_COLOR, thickness=2, circle_radius=0),
+                connection_drawing_spec=DrawingSpec(
+                    color=GREEN_COLOR, thickness=2, circle_radius=0
+                ),
                 is_drawing_landmarks=False,
             )
             draw_landmarks(
@@ -227,7 +259,9 @@ def draw_landmarks_on_image(rgb_image, detection_result, draw_subsets=False):
                 landmark_list=hand_landmarks,
                 connections=HAND_PINKY_FINGER_CONNECTIONS,
                 landmark_drawing_spec=None,
-                connection_drawing_spec=DrawingSpec(color=RED_COLOR, thickness=2, circle_radius=0),
+                connection_drawing_spec=DrawingSpec(
+                    color=RED_COLOR, thickness=2, circle_radius=0
+                ),
                 is_drawing_landmarks=False,
             )
             # Draw landmarks once after connections for clarity.
@@ -235,15 +269,21 @@ def draw_landmarks_on_image(rgb_image, detection_result, draw_subsets=False):
                 image=annotated_image,
                 landmark_list=hand_landmarks,
                 connections=None,
-                landmark_drawing_spec=DrawingSpec(color=WHITE_COLOR, thickness=2, circle_radius=2),
+                landmark_drawing_spec=DrawingSpec(
+                    color=WHITE_COLOR, thickness=2, circle_radius=2
+                ),
             )
         else:
             draw_landmarks(
                 image=annotated_image,
                 landmark_list=hand_landmarks,
                 connections=HAND_CONNECTIONS,
-                landmark_drawing_spec=DrawingSpec(color=GREEN_COLOR, thickness=2, circle_radius=2),
-                connection_drawing_spec=DrawingSpec(color=BLUE_COLOR, thickness=2, circle_radius=0),
+                landmark_drawing_spec=DrawingSpec(
+                    color=GREEN_COLOR, thickness=2, circle_radius=2
+                ),
+                connection_drawing_spec=DrawingSpec(
+                    color=BLUE_COLOR, thickness=2, circle_radius=0
+                ),
             )
 
         # Get the top left corner of the detected hand's bounding box
@@ -260,13 +300,33 @@ def draw_landmarks_on_image(rgb_image, detection_result, draw_subsets=False):
             (text_x, text_y),
             cv2.FONT_HERSHEY_DUPLEX,
             FONT_SIZE,
-            HANDEDNESS_TEXT_COLOR,
+            MAGENTA_COLOR,
             FONT_THICKNESS,
             cv2.LINE_AA,
         )
 
     return annotated_image
 
+
+# --------------------------------------------------------------------------------
+
+# Path to the model file
+model_path = pathlib.Path("models/hand_landmarker.task")
+
+# Check if the model file exists, if not, download it
+if not model_path.exists():
+    url = "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task"
+    print()
+    print(f"Downloading model from {url}...")
+    urllib.request.urlretrieve(url, model_path)
+    print(f"Model downloaded and saved as {model_path}")
+
+# Initialize MediaPipe HandLandmarker
+base_options = base_options_module.BaseOptions(model_asset_path=str(model_path))
+options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
+detector = vision.HandLandmarker.create_from_options(options)
+
+# --------------------------------------------------------------------------------
 
 # Open webcam video stream
 cap = cv2.VideoCapture(1)
